5bb127067d43867bb81915368c8c3a6d2e0dd39d
yiqunchen317 <yiqun.chen@wolves.northern.edu>
2026-02-24 13:30:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Vary", "Origin");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key, X-Tianqing-Key, X-User-Name");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key, X-Tianqing-Key, X-Creator-Key, X-User-Name");
     res.setHeader("Access-Control-Allow-Credentials", "true");
   }
 
